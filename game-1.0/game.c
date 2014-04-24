4eb155703067246ba1682a5ae516037e1a2d502b
Kristian Våge <krisvaage@gmail.com>
2014-04-24 13:18:17 +0200

    Removes query_ioctl from game.c

--- game-1.0/game.c
+++ game-1.0/game.c
@@ -5,7 +5,6 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
-#include "query_ioctl.h"
 static i = 0;
 void gamepad_handler(int signum){
 	printf("signum: %d\n",signum);
@@ -32,7 +31,6 @@ int main(void){
 	while(1)
 	{
 		signal(SIGIO, &gamepad_handler);	
-		query_gamepad_t q = {.led = 0, .buttons = 0};
 
 		//if(ioctl(fd, WRITE2GAMEPAD, &q)==-1)
 			//printf("error\n");
