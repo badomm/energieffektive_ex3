3996fcb9e51e51906d76032865d8f6b75d588889
Kristian Våge <krisvaage@gmail.com>
2014-04-25 09:51:36 +0200

    Revert "Removes query_ioctl from game.c"
    This reverts commit 4eb155703067246ba1682a5ae516037e1a2d502b.
    
    Conflicts:
    	game-1.0/game.c

--- game-1.0/game.c
+++ game-1.0/game.c
@@ -5,7 +5,9 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
+#include "query_ioctl.h"
 #include "screen.h"
+static i = 0;
 
 static int i = 0;
 void gamepad_handler(int signum){
@@ -36,6 +38,7 @@ int main(void){
 	while(1)
 	{
 		signal(SIGIO, &gamepad_handler);
+		query_gamepad_t q = {.led = 0, .buttons = 0};
 
 		//if(ioctl(fd, WRITE2GAMEPAD, &q)==-1)
 			//printf("error\n");
