ae43b61a507dbf86a71304f5223e11b8365ae899
Kristian Våge <krisvaage@gmail.com>
2014-04-27 16:46:08 +0200

    -categorized functions in driver. -removed debug printing -deleted files not in use anymore -commented and tide up driver-gamepad.c

--- driver-gamepad-1.0/driver-gamepad.c
+++ driver-gamepad-1.0/driver-gamepad.c
@@ -23,7 +23,6 @@
 #include <linux/slab.h>
 
 #include <linux/interrupt.h>
-#include "query_ioctl.h"
 #include "efm32gg.h"
 
 
@@ -36,104 +35,66 @@
 #define GPIO_EXT_MEM_REGION_LENGTH 0xc
 #define CMU_MEM_REGION_LENGTH 0x44
 
+///////////
+//Storage variables
+//////////
 static dev_t first; 		//Global variable for the first device number
 static struct cdev char_device;	//Variable for character device structure
 static struct class *device_class; 	//Global variable for the device class
-static struct task_struct *pid_type  = 0;
+
+static struct task_struct *pid_type  = 0;   //information about current active program
+static struct siginfo signal_info;                 //signal info
+
+static char gamepad_button_status = 0;      //storage for gamepad data
 
 
-static char gamepad_button_status = 0;
-
-static int send_signal(void){
-	struct siginfo signal_info;
-	printk("Sending signal\n");
-	//Setting up signal info
-	memset(&signal_info, 0, sizeof(struct siginfo));
-	signal_info.si_signo = SIGIO;
-	signal_info.si_code = SI_QUEUE;
-	signal_info.si_int = 1234;
-	printk("Actual sending\n");
-	//Do the actuall sending of signal
-	if(pid_type == 0){
-		printk("Sending signal: no PID\n");
-		return -1;
-	}
-	printk("PID exist\n");
-	if(send_sig_info(SIGIO, &signal_info, pid_type) < 0){
-		printk("Sending signal failed\n");
-		return -1;
-	}
-	printk("Gamepad: signal sent! \n");
-	return 0;
-}
-
-static irq_handler_t  gamepad_interrupt_handler(int irq, void *dumme, struct pt_regs *regs){
-	printk("Gamepad_ intterrupt handler: Received Interrupt  %d \n",*GPIO_PC_DIN);	
-	gamepad_button_status = (char)*GPIO_PC_DIN;
-	send_signal();
-	*GPIO_IFC = 0xff; //clear intermrupt
-	printk("Gamepad_ intterrupt handler: Interrupt Handled\n");
-	return (irq_handler_t) IRQ_HANDLED;
-}
+/////////////
+//Prototyes
+/////////////
+void setup_GPIO();
+static int send_signal(void);
+static irq_handler_t  gamepad_interrupt_handler(int irq, void *dumme, struct pt_regs *regs);
+static int requestIRQ(void);
+static void freeIRQ(void);
+static void setup_signal();
 
 
-static int setup_hardware_access_and_interrupt(void){
-	int status;	
-	printk("Setting up interrupts\n");
+//////////
+//Char drive interaction functions (open, release, read)
+//
+//////////////
 
-
-	status = request_irq(IRQ_N_GPIO_ODD,(irq_handler_t) gamepad_interrupt_handler,0,IRQ_NAME_GPIO_ODD, NULL);
-	if(status != 0){
-		printk("IRQ GPIO ODD request failed");
-		return status;
-	}
-	status = request_irq(IRQ_N_GPIO_EVEN,(irq_handler_t) gamepad_interrupt_handler,0,IRQ_NAME_GPIO_EVEN, NULL);
-	if(status != 0){
-		free_irq(IRQ_N_GPIO_ODD, NULL);
-		printk("IRQ GPIO EVEN request failed");	
-		return status;
-	}
-
-	return 0;
-}
-
-static void clean_hardware_access_and_interrupt(void){
-	free_irq(IRQ_N_GPIO_EVEN, NULL);
-	free_irq(IRQ_N_GPIO_ODD, NULL);
-
-	printk("cleaned interrupts\n");
-}
-
+//only one application at a time can access the gamepad
 static int gamepad_open(struct inode *i, struct file *f)
 {
 	printk("Gamepad open\n");
 	pid_type = pid_task(find_vpid(current->pid), PIDTYPE_PID); 
-	if(pid_type == NULL){
-		printk("Gamepad: no such pid\n");
+	if(pid_type == NULL){                                       
+	    printk("opening file failed \n");
 		return -1;
 	}
-
 	return 0;
 }
 static int gamepad_release(struct inode *i, struct file *f)
 {
 	kfree(pid_type);
 	pid_type = 0;
-	printk("Gamepad released\n");
 	return 0;
 }
 
 static ssize_t my_read(struct file *f, char __user *buf, size_t len, loff_t *off)
 {
-  printk(KERN_INFO "Driver read(): %d ! \n",gamepad_button_status);
   if(copy_to_user(buf, &gamepad_button_status,1) != 0){
     printk("read error \n");
     return -EFAULT;
   }
-
   return 1;
 }
 
+//////////
+//Char drive setup and cleaning
+//
+//////////////
 
 static struct file_operations gamepad_fileOps = 
 {
@@ -142,6 +103,22 @@ static struct file_operations gamepad_fileOps =
 	.release = gamepad_release,
 	.read = my_read
 };
+
+static void __exit gamepad_cleanup(void)
+{
+	//clean up pointers
+	kfree(pid_type);
+	//Stop interrupts
+	freeIRQ();
+	//free memory region
+	release_mem_region(GPIO_PC_BASE,(resource_size_t)GPIO_MEM_REGION_LENGTH);
+	
+	//remove device from filesystem
+	cdev_del(&char_device);
+	device_destroy(device_class,first);
+	class_destroy(device_class);
+	unregister_chrdev_region(first,1);
+}
 
 static int __init gamepad_init(void)
 {
@@ -172,20 +149,58 @@ static int __init gamepad_init(void)
 	struct resource *r_GPIO_PC;
 	r_GPIO_PC = request_mem_region(GPIO_PC_BASE,(resource_size_t)GPIO_MEM_REGION_LENGTH,"gamepad");
 	if(r_GPIO_PC == NULL){
-	
 		printk("GPIO_PC mem request failed\n");
 		return -1;
 	}
 	
-	if(setup_hardware_access_and_interrupt() != 0){
-		printk("Gamepad: hardware access failed\n");
+    //request interrupts
+	if(requestIRQ() != 0){
+		printk("Failed to request IRQ\n");
 		return -1;		
 	}
 	
 	printk("Device creation finnished\n");
-	//Init GPIO 
+
+    //Config GPIO
+    setup_GPIO();   
+
+    //Setup internal variables
+	pid_type = NULL;
+	setup_signal();
 	
-	
+    printk("<Major, Minor>: <%d,%d>\n",MAJOR(first),MINOR(first));
+	printk("Gamepad module is set up and  ready to go!!!!\n");
+	return 0;
+}
+
+static int requestIRQ(void){
+	int status;	
+	status = request_irq(IRQ_N_GPIO_ODD,(irq_handler_t) gamepad_interrupt_handler,0,IRQ_NAME_GPIO_ODD, NULL);
+	if(status != 0){
+		printk("IRQ GPIO ODD request failed");
+		return status;
+	}
+	status = request_irq(IRQ_N_GPIO_EVEN,(irq_handler_t) gamepad_interrupt_handler,0,IRQ_NAME_GPIO_EVEN, NULL);
+	if(status != 0){
+		free_irq(IRQ_N_GPIO_ODD, NULL);
+		printk("IRQ GPIO EVEN request failed");	
+		return status;
+	}
+
+	return 0;
+}
+
+static void freeIRQ(void){
+	free_irq(IRQ_N_GPIO_EVEN, NULL);
+	free_irq(IRQ_N_GPIO_ODD, NULL);
+
+}
+
+
+
+
+void setup_GPIO(){
+    //Init GPIO 	
 	*GPIO_PC_MODEL = 0x33333333;			//Set pins as input
 	*GPIO_PC_DOUT  = 0xff;
 
@@ -194,33 +209,43 @@ static int __init gamepad_init(void)
 	*GPIO_EXTIFALL = 0xff;
 	*GPIO_EXTIRISE = 0xff;
 	*GPIO_IEN = 0xff;
+}
 
+///////////
+// Signal and intterrupt handling
+// Receive HW intterrupts and notify user space
+///////////////
+static irq_handler_t  gamepad_interrupt_handler(int irq, void *dumme, struct pt_regs *regs){
+	gamepad_button_status = (char)*GPIO_PC_DIN;
+	send_signal();
+	*GPIO_IFC = 0xff; //clear interrupt
+	return (irq_handler_t) IRQ_HANDLED;
+}
 
-	printk("Gamepad: interrupts up and running\n");
+static void setup_signal(){
+	//Setting up signal info
+	memset(&signal_info, 0, sizeof(struct siginfo));
+	signal_info.si_signo = SIGIO;
+	signal_info.si_code = SI_QUEUE;
+	signal_info.si_int = 1234;
+}
 
-	kfree(pid_type);
-	pid_type = 0;
-	printk("<Major, Minor>: <%d,%d>\n",MAJOR(first),MINOR(first));
-	printk("Gamepad module is set up and  ready to go!!!!\n");
+static int send_signal(void){
+
+	//Do the actuall sending of signal
+	if(pid_type == 0){
+		return -1;
+	}
+	if(send_sig_info(SIGIO, &signal_info, pid_type) < 0){
+		return -1;
+	}
 	return 0;
 }
 
 
-static void __exit gamepad_cleanup(void)
-{
-	printk("removing gamepad module\n");
-	kfree(pid_type);
-	//Stop interrupts
-	clean_hardware_access_and_interrupt();
-	release_mem_region(GPIO_PC_BASE,(resource_size_t)GPIO_MEM_REGION_LENGTH);
-	cdev_del(&char_device);
-	device_destroy(device_class,first);
-	class_destroy(device_class);
-	unregister_chrdev_region(first,1);
-	printk("gamepad module removed\n");
-}
-
-
+///////
+//
+////////
 module_init(gamepad_init);
 module_exit(gamepad_cleanup);
 
--- game-1.0/playgrid.c
+++ game-1.0/playgrid.c
@@ -54,14 +54,12 @@ playgrid_legal move_object(int x, int y, playgrid_direction direction, PlayGrid*
 		break;
 	}
 	
-	printf("move_object: move legal\n");
-	
 	//check if new values are within legal range (within grid)
 	if(y_new < 0 || y_new >= grid->y ||
 	   x_new < 0 || x_new >= grid->x){
 		return ILLEGAL;
 	}
-	printf("move_object: move is inside field\n");
+
 	//get objects and squares for new and current position
 	current_object = get_Playgrid_object(x,y,grid);
         
@@ -73,25 +71,21 @@ playgrid_legal move_object(int x, int y, playgrid_direction direction, PlayGrid*
 		case GOAL:case NORMAL:
 		default: break;
 	}
-	printf("move_object: move does not walk into wall\n");
+
 	
 	//move depends on object being moved
 	switch(current_object){
 		case EMPTY:	//cannot move something empty
-			printf("move_object: moving EMPTY\n");
 			return ILLEGAL;		
 		break;	
 		case PLAYER:
-			printf("move_object: moving PLAYER\n");
 			return move_player(direction, x_new, y_new, grid,updated);
 		break;	
 		case ROCK:
-			printf("move_object: moving ROCK\n");
 			return move_rock(x,y,x_new,y_new,grid,updated);		
 		break;
 		
 		default:
-			printf("moving object, DEFAULT CASE\n");
 			return ILLEGAL;		
 		break;
 	}
