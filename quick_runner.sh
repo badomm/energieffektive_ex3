5ea573c24a914d7985439b4e27e0e82791aa114a
Kristian Våge <krisvaage@gmail.com>
2014-04-27 17:08:58 +0200

    Fixed runners

--- quick_runner.sh
+++ quick_runner.sh
@@ -1,6 +1,9 @@
-ptxdist clean game
-ptxdist compile game 
-ptxdist clean driver-gamepad
-ptxdist compile driver-gamepad
+ptxdist clean game &&
+ptxdist clean driver-gamepad &&
+ptxdist compile game &&
+ptxdist compile driver-gamepad &&
+ptxdist targetinstall game &&
+ptxdist targetinstall driver-gamepad &&
+ptxdist image root.romfs &&
 ptxdist test flash-rootfs
 exec miniterm.py -b 115200 -p /dev/ttyS0
--- runner.sh
+++ runner.sh
@@ -1,5 +1,5 @@
-ptxdist clean game
-ptxdist clean driver-gamepad
-ptxdist images
-ptxdist test flash-all
+ptxdist clean game &&
+ptxdist clean driver-gamepad &&
+ptxdist images &&
+ptxdist test flash-all &&
 exec miniterm.py -b 115200 -p /dev/ttyS0
